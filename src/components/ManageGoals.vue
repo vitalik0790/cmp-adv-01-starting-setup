3a776b482e172b5d90d3a03671e6243af240a681
Vitalii Kovalchuk <vitaliikovalchuk55@gmail.com>
2021-04-21 18:25:59 +0300

    slots

--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -1,6 +1,39 @@
 <template>
   <div>
     <h2>Manage Goals</h2>
-    <input type="text" />
+    <input type="text" ref="goal" />
+    <button @click="setGoal">Set Goal</button>
+    <teleport to="body">
+      <error-alert v-if="inputIsValid">
+        <h1>Input is invalid!</h1>
+        <p>Please enter at least a few characters...</p>
+        <button @click="confirmError">Okay</button>
+      </error-alert>
+    </teleport>
   </div>
 </template>
+
+<script>
+import ErrorAlert from "./ErrorAlert";
+export default {
+  components: {
+    ErrorAlert,
+  },
+  data() {
+    return {
+      inputIsValid: false,
+    };
+  },
+  methods: {
+    setGoal() {
+      const enteredValue = this.$refs.goal.value;
+      if (enteredValue === "") {
+        this.inputIsValid = true;
+      }
+    },
+    confirmError() {
+      this.inputIsValid = false;
+    },
+  },
+};
+</script>

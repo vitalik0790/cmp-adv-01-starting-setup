b910ab2b397e0e1a25dc8d42fb1dd34736868c11
Vitalii Kovalchuk <vitaliikovalchuk55@gmail.com>
2021-04-21 19:47:45 +0300

    done

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@
 </template>
 
 <script>
-import TheHeader from "./components/TheHeader";
+import TheHeader from "./components/layout/TheHeader";
 import ManageGoals from "./components/ManageGoals";
 import ActiveGoals from "./components/ActiveGoals";
 

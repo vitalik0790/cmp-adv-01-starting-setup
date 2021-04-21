0efe81efda9d1d4915d065e521b67856d0c54596
Vitalii Kovalchuk <vitaliikovalchuk55@gmail.com>
2021-04-21 17:45:39 +0300

    keep-alive

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <TheHeader />
+    <!-- <BadgeList />
+    <user-info
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></user-info>
+    <course-goals #default="slotProps">
+      <h2>{{ slotProps.item }}</h2>
+      <p>{{ slotProps.anotherProp }}</p>
+    </course-goals> -->
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
+    <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals> -->
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+import TheHeader from "./components/TheHeader";
+import ManageGoals from "./components/ManageGoals";
+import ActiveGoals from "./components/ActiveGoals";
+
+export default {
+  components: {
+    TheHeader,
+    ManageGoals,
+    ActiveGoals,
+  },
+
+  data() {
+    return {
+      selectedComponent: "active-goals",
+      activeUser: {
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
+      },
+    };
+  },
+
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp;
+    },
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
